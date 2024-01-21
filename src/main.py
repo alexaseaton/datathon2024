243bd71c8d5c03ebfb8d75075490dec4d6aac6cd
melanie-09 <143124193+melanie-09@users.noreply.github.com>
2024-01-20 21:16:39 -0600

    woo

--- src/main.py
+++ src/main.py
@@ -2,7 +2,30 @@ import numpy as np
 import pandas as pd 
 import seaborn as sns
 import matplotlib.pyplot as plt
-data = np.load('p00_n1_X.npy')
-for i in range(len(data)):
-    plt.img(data[i], cmap='gray')
-    plt.show()
+from textwrap import wrap
+
+
+#Parse and load the file
+data = np.fromfile('eval_a_NEW_X.npy',dtype=float)
+dataframe = pd.DataFrame(data)
+data1 = np.load('eval_a_NEW_X.npy')
+newdata = pd.DataFrame()
+
+#Initialize a placeholder array and index to skip every 15 characters
+array = np.zeros(shape=(23400016,5))
+num = 15
+
+#Iterate through the file, splitting up the data into more columns
+for row in data:
+    row_num = 0
+    row_list = row.tolist() #Convert to list, then string
+    row_as_str = str(row_list)
+    fuck = wrap(row_as_str, 15) #Split each 
+    for element in fuck:
+        col_num = 0
+        array[row_num,col_num] = element
+        col_num += 1
+        # newdata.loc[len(newdata)] = split
+    row_num += 1
+
+print(array)
